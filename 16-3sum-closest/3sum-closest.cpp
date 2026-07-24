248a8002c4f1bec8b8d0d68c11696abc941fd444
Abhijeet200705 <abhijeetbeheraab07c@gmail.com>
2026-07-24 11:18:03 +0530

    Time: 19 ms (13.20%) | Memory: 13.9 MB (81.81%) - LeetSync

--- 16-3sum-closest/3sum-closest.cpp
+++ 16-3sum-closest/3sum-closest.cpp
@@ -11,7 +11,7 @@ public:
         
 
         for(int i=0;i<n-2;i++){
-            if (i>0 && nums[i]==nums[i-1]) continue;
+            
             int l=i+1;
             int r=n-1;
             while(l<r){
